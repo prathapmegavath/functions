6137bf5633474a2e08a31ab167109e85659ce47c
prathapmegavath <prathapsinghmegavath@gmail.com>
2024-12-19 17:09:59 +0530

    ps problems are solved

---
+++ index.js
@@ -0,0 +1,47 @@
+// var a= -prompt("enter a number");
+ 
+// if(a<=7){
+//         switch(a){
+//         case 1:
+//             console.log("monday");
+//             break;
+//         case 2:
+//             console.log(" tuesday");
+//             break;
+//         case 3:
+//             console.log("wednesday");
+//             break;
+//         case 4:
+//             console.log("thrusday");
+//             break;
+//         case  5:
+//             console.log("friday");
+//             break;
+//         case 6:          
+//             console.log("saturday");
+//             break;
+//         case 7:
+//             console.log("daysun");
+//             break;    
+//         default:
+//             console.log("i am here")
+//         }
+
+//     }
+//     else
+//     {
+//         console.log("enter a proper input")
+//     }
+// ----------------------------------------------------------------
+// var arr=[ 1,2,"king",[true,undefined,1]]
+// console.log(arr.length)
+// console.log(arr)
+// var obj={
+//     name:"parthu",
+//     Age:"32",
+//     college:"cmr ec engeneering college"
+// }
+// console.log(obj)
+// ----------------------------------------------------------------------
+var arr=["apple","banana","custerdapple","dragonfruit",[1,2,3],["vamshi","ravi"]]
+console.log(arr[arr.length-1][1])
